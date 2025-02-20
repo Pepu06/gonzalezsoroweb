215834c79aa5a7791a1124eb535e387620b68fab
Pepu06 <pedrogonzalezsoro@gmail.com>
2025-02-20 20:33:38 -0300

    npm run build funca

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +9,8 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   publicDir: './public',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
